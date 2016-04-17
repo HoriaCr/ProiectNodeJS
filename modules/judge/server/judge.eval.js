02cfbac161abf6ad74c27e7dc5509ad86cd56a6f
dan-ghita <dan_ghita_95@yahoo.com>
2016-04-17 15:35:13 +0300

    Fix server evaluator bug

--- modules/judge/server/judge.eval.js
+++ modules/judge/server/judge.eval.js
@@ -4,10 +4,10 @@ var cp = require('child_process');
 var path = require('path');
 
 
-var runTest = function(interpreter, filename, options, expected, test) {
+var runTest = function (interpreter, filename, options, expected, test) {
     // start time
     var start = process.hrtime();
-    var child = cp.spawnSync(interpreter, [filename], options); 
+    var child = cp.spawnSync(interpreter, [filename], options);
     if (child.stderr !== '') {
         return {
             'status': 'Compilation error: ' + child.stderr,
@@ -15,13 +15,13 @@ var runTest = function(interpreter, filename, options, expected, test) {
             'test': 0
         };
     }
-    
+
     // ignore trailing newlines when comparing result
     var output = child.stdout.replace(/\n$/, "");
-    
-    var deltaMs = process.hrtime(start)[1] / 1000000; 
+
+    var deltaMs = process.hrtime(start)[1] / 1000000;
     // time it takes for execution
-    
+
     var success = ['Wrong answer', 'Accepted', 'Time limit exceeded'];
     if (deltaMs > child.options.timeout) {
         return {
@@ -38,47 +38,48 @@ var runTest = function(interpreter, filename, options, expected, test) {
 };
 
 
-
 /*
-TODO: support compiled languages
-var langCompilers = {
-    'c' : ['gcc', ['-O2', '-Wextra', '-o']],
-    'c++11' : ['g++-5', ['-std=c++11', '-O3', '-Wextra', '-o']],
-    'java': ['javac', []]
-};
-*/
+ TODO: support compiled languages
+ var langCompilers = {
+ 'c' : ['gcc', ['-O2', '-Wextra', '-o']],
+ 'c++11' : ['g++-5', ['-std=c++11', '-O3', '-Wextra', '-o']],
+ 'java': ['javac', []]
+ };
+ */
 
-var evalProblem = function(submission, problem) {
-
+var evalProblem = function (submission, problem) {
     var options = {
         timeout: problem.timeLimit,
         input: problem.examples[0].input,
         encoding: 'utf8'
     };
-    
+
     var langInterpreters = {
-        'python3': 'python3', 
-        'python2.7': 'python', 
-        'ruby': 'ruby', 
+        'python3': 'python3',
+        'python2.7': 'python',
+        'ruby': 'ruby',
         'javascriptv8': 'node'
     };
-   
 
     var language = submission.language;
     var data = submission.submission;
     // generate unique file
     var filename = 'test' + crypto.randomBytes(4).readUInt32LE(0);
-    var file = path.join(__dirname, 'data', filename);
+    var dataDir = path.join(__dirname, 'data');
+    var file = path.join(dataDir, filename);
 
+    if(!fs.existsSync(dataDir))
+        fs.mkdir(dataDir);
+    
     fs.writeFileSync(file, data);
 
     var result = runTest(langInterpreters[language], file, options,
-            problem.examples[0].output);
+        problem.examples[0].output);
     if (result[0] === 'Compilation error') {
         return result;
     }
     var testIndex = 0;
-    var results = problem.tests.map(function(test) {
+    var results = problem.tests.map(function (test) {
         options.input = test.input;
         testIndex++;
         return runTest(langInterpreters[language], file, options,
@@ -91,13 +92,12 @@ var evalProblem = function(submission, problem) {
 };
 
 
-
-var evaluateProblem = function(submission, problem) {
+var evaluateProblem = function (submission, problem) {
     var res = evalProblem(submission, problem);
     var ret = {};
     if (res[0] === 'Compilation error') {
         ret = {
-            'evaluationStatus': 'Compilation Error' ,
+            'evaluationStatus': 'Compilation Error',
             'results': []
         };
     } else {
